943832b65d2996ef30f58aeb5f74de6603e4a2fc
Pop0097 <dhruvrawat09@gmail.com>
2020-06-14 11:53:06 -0400

    Update

---
+++ js/logo.js
@@ -0,0 +1,13 @@
+
+window.onload = updateLogoSize;
+window.onresize = updateLogoSize;
+
+function updateLogoSize(){
+    var w = window.innerWidth;
+    console.log("Width " , w);
+    if(w < 756) {
+        document.getElementById('logo-image').src = "../images/short-logo.png";
+    } else {
+        document.getElementById('logo-image').src = "../images/logo-long.png";
+    }
+}

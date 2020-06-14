f499d311511a9e792d30e85d658d04164a6780f3
Pop0097 <dhruvrawat09@gmail.com>
2020-06-14 11:05:54 -0400

    Update

--- js/logout.js
+++ js/logout.js
@@ -4,6 +4,7 @@
     e.preventDefault();
     firebase.auth().signOut().then(function(){
       console.log("logout successful");
+      window.location.replace("index.html");
     }).catch(function(error){
       console.log("logout unsuccessful!");
     });
